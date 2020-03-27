bfa39772a83c8b3a41b65f808fa0f824d9f212d1
“Krista <stadler.krista@gmail..com>
2020-03-27 09:55:19 -0600

    Finished User Story 2

---
+++ spec/features/professors/user_can_see_professors_students_spec.rb
@@ -0,0 +1,41 @@
+require 'rails_helper'
+
+RSpec.describe "as a visitor", type: :feature do
+  it "can visit a professor show page and see the names of all of that professor's students" do
+
+  snape = Professor.create(name: "Severus Snape", age: 45, specialty: "Potions")
+  hagarid = Professor.create(name: "Rubus Hagarid", age: 38 , specialty: "Care of Magical Creatures")
+  lupin = Professor.create(name: "Remus Lupin", age: 49 , specialty: "Defense Against The Dark Arts")
+
+  harry = Student.create(name: "Harry Potter" , age: 11 , house: "Gryffindor" )
+  malfoy = Student.create(name: "Draco Malfoy" , age: 12 , house: "Slytherin" )
+  longbottom = Student.create(name: "Neville Longbottom" , age: 11 , house: "Gryffindor" )
+
+  ProfessorStudent.create!(student_id: harry.id, professor_id: snape.id)
+  ProfessorStudent.create!(student_id: harry.id, professor_id: hagarid.id)
+  ProfessorStudent.create!(student_id: harry.id, professor_id: lupin.id)
+  ProfessorStudent.create!(student_id: malfoy.id, professor_id: hagarid.id)
+  ProfessorStudent.create!(student_id: malfoy.id, professor_id: lupin.id)
+  ProfessorStudent.create!(student_id: longbottom.id, professor_id: snape.id)
+
+  visit "/professors/#{snape.id}'"
+
+  expect(page).to have_content("Harry Potter")
+  expect(page).to have_content("Neville Longbottom")
+  expect(page).to_not have_content("Draco Malfoy")
+
+
+  visit "/professors/#{hagarid.id}'"
+
+  expect(page).to have_content("Harry Potter")
+  expect(page).to have_content("Draco Malfoy")
+  expect(page).to_not have_content("Neville Longbottom")
+
+  visit "/professors/#{lupin.id}'"
+
+  expect(page).to have_content("Harry Potter")
+  expect(page).to have_content("Draco Malfoy")
+  expect(page).to_not have_content("Neville Longbottom")
+
+  end
+end
